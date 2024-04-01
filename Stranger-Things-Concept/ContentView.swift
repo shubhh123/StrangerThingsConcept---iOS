fd89b8915283aa9d90117979a0a9595d549fb9a3
shubhh123 <shubhh1181@gmail.com>
2024-04-01 19:09:45 +0530

    Initial Commit

---
+++ Stranger-Things-Concept/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stranger-Things-Concept
+//
+//  Created by Shubham on 01/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stranger-Things-Concept/Stranger_Things_ConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stranger_Things_ConceptApp.swift
+//  Stranger-Things-Concept
+//
+//  Created by Shubham on 01/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Stranger_Things_ConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
